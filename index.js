5d253d928b81bdae67a8e1a7c2244759e3f90bc7
Joy Phoebe <joyapisi@gmail.com>
2023-03-27 14:32:35 +0300

    fix linter errors

--- index.js
+++ index.js
@@ -8,17 +8,6 @@ addBookBtn.addEventListener('click', () => {
   books.addBook();
 });
 
-// const date = new Date();
-
-// const secondes = date.getSeconds();
-// const hour = date.getHours();
-// const minutes = date.getMinutes();
-// const day = date.getDate();
-// const month = date.getMonth() + 1;
-// const year = date.getFullYear();
-// const ampm = hour >= 12 ? 'pm' : 'am';
-
-// const currentDate = `${day}.${month}.${year}, ${hour}:${minutes}:${secondes} ${ampm}`;
 const todayDate = document.getElementById('date');
 todayDate.innerHTML = (`${DateTime.now().toLocaleString(DateTime.DATETIME_MED)}`);
-// todayDate.innerHTML = `${DateTime.now().toLocaleString(DateTime.DATETIME_MED)}`;
+
